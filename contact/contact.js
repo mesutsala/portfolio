d502684f7c75e5490527a7be2b005d0779fc6b11
Mesut <62920044+mesutsala@users.noreply.github.com>
2021-10-10 17:06:21 -0400

    Add files via upload

---
+++ contact/contact.js
@@ -0,0 +1,14 @@
+//NAV BAR
+const openBurger = document.getElementById("navbar");
+const closeBurger = document.getElementById("navbar");
+
+function openNav() {
+  openBurger.style.width = "100%";
+  openBurger.style.backgroundColor = "rgba(0,0,0,1)";
+}
+
+function closeNav() {
+  closeBurger.style.width = "0";
+  closeBurger.style.backgroundColor = "transparent";
+}
+//NAV BAR
